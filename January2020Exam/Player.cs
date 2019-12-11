59cec965f58f7a4c6f684a7f144a73d0e94b16b0
grok23 <46896171+grok23@users.noreply.github.com>
2019-12-11 09:49:38 +0000

    Add project files.

---
+++ January2020Exam/Player.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace January2020Exam
+{
+    //declare positions as enum
+    public enum Position { GoalKeeper, Defender, Midfielder, Forward }
+
+    class Player : IComparable
+    {
+        public string FirstName { get; set; }
+        public string Surname { get; set; }
+        public Position PreferredPosition { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public int Age { get; set; }
+    
+    }
+}
