10ab40384f106c5725886ec85a86d2da985d1ec0
grok23 <46896171+grok23@users.noreply.github.com>
2019-12-11 10:13:52 +0000

    added age check

--- January2020Exam/Player.cs
+++ January2020Exam/Player.cs
@@ -15,7 +15,17 @@ namespace January2020Exam
         public string Surname { get; set; }
         public Position PreferredPosition { get; set; }
         public DateTime DateOfBirth { get; set; }
-        public int Age { get; set; }
-    
+        private int _age;
+        public int Age
+        {
+            get
+            {
+                _age = DateTime.Now.Year - DateOfBirth.Year;
+                if (DateOfBirth.DayOfYear > DateTime.Now.DayOfYear)
+                    _age--;
+                return _age;
+            }
+        }
+
     }
 }
